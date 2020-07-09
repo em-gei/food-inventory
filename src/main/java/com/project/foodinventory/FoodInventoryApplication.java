4bde37b7bf1f2972ec1733e361d2089765ec0962
Emgei <jacopomattei.fi@gmail.com>
2020-07-09 11:20:39 +0200

    first commit

---
+++ src/main/java/com/project/foodinventory/FoodInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.project.foodinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodInventoryApplication.class, args);
+	}
+
+}

bd4c674f4bd79715b81b71749513cf51b55e5e8c
Emgei <jacopomattei.fi@gmail.com>
2020-07-11 15:52:07 +0200

    #3 Implemented food page with CRUD operations

---
+++ src/main/java/com/project/foodinventory/repositories/FoodRepository.java
@@ -0,0 +1,9 @@
+package com.project.foodinventory.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.project.foodinventory.models.Food;
+
+public interface FoodRepository extends JpaRepository<Food, Integer> {
+
+}
